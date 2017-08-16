ea0c6b9f75c539c20c0aa650eaeec250c8408505
Marylette Roa <maryletteroa@gmail.com>
2017-08-16 04:11:39 +0000

    upload script

---
+++ scripts/xml_blast_getqueryseq.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+'''
+@MaryletteRoa
+7 March 2017
+'''
+
+from sys import argv
+from Bio.Blast import NCBIXML
+
+blast = NCBIXML.parse(open(argv[1]))
+outf = open('hspQseq.faa','w')
+
+for record in blast:
+   title = record.query
+   if record.alignments:
+      for recHit in record.alignments:
+         for hsp in recHit.hsps:
+           start = hsp.query_start
+           end = hsp.query_end
+           query_seq = hsp.query
+           fas_header = '>{}_{}_{}'.format(title,start,end)
+           fasta = '{}\n{}'.format(fas_header,query_seq)
+           outf.write(fasta+'\n')
+         break
+
+outf.close()
+blast.close()
