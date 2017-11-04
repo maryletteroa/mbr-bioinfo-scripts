4328520c01eab2196ee06e66a16bf5abfed0af61
Marylette Roa <maryletteroa@gmail.com>
2017-11-04 16:34:24 +0800

    also output header map file (map.txt)

--- scripts/fasta_rename_contigs.py
+++ scripts/fasta_rename_contigs.py
@@ -4,7 +4,7 @@
 # @Author: Marylette Roa
 # @Date:   2017-11-04 16:12:26
 # @Last Modified by:   Marylette Roa
-# @Last Modified time: 2017-11-04 16:20:04
+# @Last Modified time: 2017-11-04 16:33:55
 
 
 
@@ -17,13 +17,17 @@ except IndexError:
     print(help)
     exit()
 
-headercount = 0
-outf_name = '.'.join(argv[1].split('.')[0:-1])  + '.renamed.fasta'
-outf = open(outf_name,'w')
 
 with open(fasta_file) as inf:
     sequences = [line for line in inf.read().split('>')[1:]]
     fastas = {sequence.split('\n')[0]:''.join(sequence.split('\n')[1:]).upper() for sequence in sequences}
+
+
+outf_name = '.'.join(argv[1].split('.')[0:-1])  + '.renamed.fasta'
+outf = open(outf_name,'w')
+
+map_outf_name = 'map.txt'
+map_outf = open(f'{map_outf_name}','w')
 
 counter = 0
 for fasta in fastas:
@@ -31,8 +35,10 @@ for fasta in fastas:
     header = f'contig_{counter}'
     sequence = fastas[fasta]
     fasta_p = f'>{header}\n{sequence}'
+    map_p = f'{fasta}\t{header}'
     print(fasta_p, file=outf, end='\n')
+    print(map_p, file=map_outf, end='\n')
 
-print(f'Output: {outf_name}')
+print(f'Output: {outf_name}, {map_outf_name}')
 outf.close()
 
