d09f488114e58c0c26ad21409965f1187192c49e
Marylette Roa <mroa@DDHP-P1-server>
2018-03-02 10:38:48 +0800

    update xml blast parser

--- scripts/xml_blast_getqueryseq.py
+++ scripts/xml_blast_getqueryseq.py
@@ -12,13 +12,15 @@ blast = NCBIXML.parse(open(argv[1]))
 outf = open('hspQseq.faa','w')
 
 for record in blast:
-   title = record.query
+   query_title = record.query
    if record.alignments:
       for recHit in record.alignments:
          for hsp in recHit.hsps:
            start = hsp.query_start
            end = hsp.query_end
-           query_seq = hsp.query
+           query_seq = hsp.query.replace('-','')
+           hit_title = recHit.title
+           title = f'Query: {query_title} Hit: {hit_title}'
            fas_header = '>{}_{}_{}'.format(title,start,end)
            fasta = '{}\n{}'.format(fas_header,query_seq)
            outf.write(fasta+'\n')
