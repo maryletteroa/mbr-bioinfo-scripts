ca76d764341e0f88e31f39adb77debfc576a57b8
Marylette Roa <maryletteroa@gmail.com>
2018-03-08 21:52:52 +0800

    update usage of script to remove redanduncy in fasta sequences

--- scripts/fasta_remove_redundant_seqs.py
+++ scripts/fasta_remove_redundant_seqs.py
@@ -5,7 +5,8 @@ try:
     fasta_file  = argv[1]
 except IndexError:
     about = '''
-Remove redundant fasta sequences (completely identical characters) from a multi-fasta file
+Removes redundancy in fasta sequences (fasta sequences with completely identical characters) from a multi-fasta file.
+Caution: Does not check for lettercases etc. Duplicate sequences are merged into one
 '''
     usage = f'  Usage: {argv[0].split("/")[-1]} <sequence.fasta>'
     print(about, usage)
