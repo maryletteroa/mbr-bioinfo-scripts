c1ba3a3b83ac85320c59d21bfce3b18e47608571
Marylette Roa <mroa@DDHP-P1-server>
2018-03-09 10:58:14 +0800

    add script convert embl file to fasta

---
+++ scripts/embl2fasta.py
@@ -0,0 +1,31 @@
+from sys import argv, exit
+from Bio import SeqIO
+
+
+try:
+    fasta_file = argv[1]
+except IndexError:
+    about = '''
+Converts embl file to fasta
+'''
+    usage = f'''
+Usage: {argv[0].split('/')[-1]} <sequence.embl>
+'''
+    help = f'{about}{usage}'
+    print(help)
+    exit()
+
+
+outname = f'{fasta_file}.fasta'
+outf = open(outname,'w')
+
+with open(fasta_file) as inf:
+    for record in SeqIO.parse(inf, 'embl'):
+        header = record.id
+        sequence = record.seq
+        fasta = f'>{header}\n{sequence}'
+        print(fasta,file=outf)
+outf.close()
+
+
+print(f'Output: {outname}')
