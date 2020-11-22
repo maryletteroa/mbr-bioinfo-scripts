1d8f37c82e1361154afd8cf0e714bab09eea47d2
maryletteroa <maryletteroa@gmail.com>
2020-11-22 11:44:45 +0000

    print excess seqs to last file

--- scripts/fasta_split.py
+++ scripts/fasta_split.py
@@ -2,14 +2,14 @@
 # @Author: Marylette B. Roa
 # @Date:   2020-11-22 11:02:51
 # @Last Modified by:   Marylette B. Roa
-# @Last Modified time: 2020-11-22 11:32:18
+# @Last Modified time: 2020-11-22 11:44:06
 
 
 from sys import  argv, exit
 
 try:
     inn = argv[1]
-    num_files = argv[2]
+    num_files = int(argv[2])
     outn = argv[3]
 except IndexError:
     print("foo.py <in.fasta> <num_of_files_to_print> <prefix_of_output_files> ")
@@ -20,11 +20,17 @@ file_counter = 0
 with open(inn) as inf:
     fastas = inf.read().split(">")[1:]
     total_seqs = len(fastas)
-    seq_per_file = total_seqs // int(num_files)
+    seq_per_file = total_seqs // num_files
     for r in range(0, total_seqs, seq_per_file):
-        file_counter +=1
-        outn_ = f"{outn}_{file_counter}.fasta"
-        outf = open(f"{outn_}", "w")
-        sequences_to_file = ">"+">".join(fastas[r:r+seq_per_file]).rstrip()
-        print(sequences_to_file, file=outf)
-        print(f"Output: {outn_}")
+        if file_counter <= num_files:
+            file_counter +=1
+            outn_ = f"{outn}_{file_counter}.fasta"
+            outf = open(f"{outn_}", "w")
+            sequences_to_file = ">"+">".join(fastas[r:r+seq_per_file]).rstrip()
+            print(sequences_to_file, file=outf)
+            print(f"Output: {outn_}")
+        else:
+            file_counter =-1
+            sequences_to_file = ">"+">".join(fastas[r:r+seq_per_file]).rstrip()
+            print(sequences_to_file, file=outf)
+            print(f"Output: {outn_}")
